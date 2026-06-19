dc0efd4549e3a4a64178c7771d77760e2f4b070d
Kanika Panwar <kanikathakurpanwar@gmail.com>
2026-06-19 22:27:15 +0530

    Added Day_5 programs

---
+++ Day_5/program4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n, i, largest = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i=2; i<=n; i++) {
+        while(n % i == 0) {
+            largest = i;
+            n /= i;
+        }
+    }
+    printf("Largest prime factor of the number is: %d", largest);
+    return 0;
+}
