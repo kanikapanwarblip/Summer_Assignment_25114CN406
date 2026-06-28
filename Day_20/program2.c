9e3b9cacbc64234b0a340cb03b742d0ecd23ee23
Kanika Panwar <kanikathakurpanwar@gmail.com>
2026-06-28 23:52:55 +0530

    Add Day_20 programs

---
+++ Day_20/program2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, symmetric = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check if the matrix is symmetric
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("The matrix is Symmetric.");
+    else
+        printf("The matrix is Not Symmetric.");
+
+    return 0;
+}
